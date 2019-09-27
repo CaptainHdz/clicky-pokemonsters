81ffe7910b217686ada2c561abebbab964f5636e
unknown <mauro.hdz99@gmail.com>
2019-09-27 14:18:36 -0500

    Added card styling

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -5,7 +5,7 @@ import './style.css';
 
 function Card(props) {
     return(
-      <img src={props.image} status={props.clicked} id={props.id} onClick={() => props.turnTrue(props.id)}  alt={props.name} className="col-3 img-thumbnail pokemon"/>
+      <img src={props.image} status={props.clicked} id={props.id} onClick={() => props.turnTrue(props.id)}  alt={props.name} className="col-2 img-thumbnail m-2 shadow pokemon"/>
     )
 }
 
